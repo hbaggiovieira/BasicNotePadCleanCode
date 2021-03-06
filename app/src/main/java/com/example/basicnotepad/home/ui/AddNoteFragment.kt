3bc2c47a683b401d9211b7b6a1d38f3d77b6e4f4
Henrique <hbaggiovieira@gmail.com>
2021-03-06 16:00:53 -0300

    simple notepad

--- app/src/main/java/com/example/basicnotepad/home/ui/AddNoteFragment.kt
+++ app/src/main/java/com/example/basicnotepad/home/ui/AddNoteFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.basicnotepad.R
+import com.example.basicnotepad.core.repository.SharedPreferencesRepository
 import com.example.basicnotepad.home.HomeSharedViewModel
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.fragment_add_note.*
@@ -15,6 +16,10 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class AddNoteFragment : Fragment() {
     private val sharedViewModel by sharedViewModel<HomeSharedViewModel>()
+
+    private val pref by lazy {
+        SharedPreferencesRepository(requireContext())
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,19 +30,27 @@ class AddNoteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setTitle()
+        setupViews()
         setupButtons()
     }
 
     private fun setupButtons() {
         fabSaveNote.setOnClickListener{
-            sharedViewModel.note.postValue(edtTxtNote.text.toString())
+            pref.save("note", edtTxtNote.text.toString())
             Toast.makeText(requireContext(), "Anotação salva com sucesso!", Toast.LENGTH_SHORT)
                 .show()
         }
     }
 
-    private fun setTitle() {
+    private fun setupViews() {
+        val savedString = pref.getValueString("note")
+
         requireActivity().toolbar.titleLabel.text = getString(R.string.title_label_add_note_fragment)
+
+        if (savedString.isNullOrEmpty()) {
+            edtTxtNote.text.clear()
+        } else {
+            edtTxtNote.setText(savedString)
+        }
     }
 }
--- app/src/main/java/com/example/basicnotepad/home/ui/HomeFragment.kt
+++ app/src/main/java/com/example/basicnotepad/home/ui/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.basicnotepad.R
+import com.example.basicnotepad.core.repository.SharedPreferencesRepository
 import com.example.basicnotepad.home.HomeSharedViewModel
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -15,6 +16,10 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 
 class HomeFragment : Fragment() {
     private val sharedViewModel by sharedViewModel<HomeSharedViewModel>()
+
+    private val pref by lazy {
+       SharedPreferencesRepository(requireContext())
+    }
 
     private val navigator get() = findNavController()
     override fun onCreateView(
@@ -26,24 +31,23 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setTitle()
         setupButtons()
         setupViews()
     }
 
     private fun setupViews() {
-        if (sharedViewModel.note.value.isNullOrEmpty()) {
-            sharedViewModel.note.value = "Adicione a sua primeira anotação!"
+        requireActivity().toolbar.titleLabel.text = getString(R.string.title_label_home_fragment)
+
+        if (pref.getValueString("note").isNullOrEmpty()) {
+            txtNote.text = "Adicione a sua primeira anotação!"
+        } else {
+            txtNote.text = pref.getValueString("note")
         }
-        txtNote.text = sharedViewModel.note.value.toString()
     }
 
     private fun setupButtons() {
         fab.setOnClickListener {
             navigator.navigate(R.id.action_homeFragment_to_addNoteFragment)
         }
-    }
-    private fun setTitle() {
-        requireActivity().toolbar.titleLabel.text = getString(R.string.title_label_home_fragment)
     }
 }
